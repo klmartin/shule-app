ac32aca22ffd31f9503391c29c96cc0f23ec5e71
martin <martinkaboja@gmail.com>
2022-05-12 10:28:48 +0300

    initial commit

---
+++ database/seeders/StatesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\State;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatesTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('states')->delete();
+
+        $states = [
+            'Arusha', 'Dar es Salaam', 'Dodoma', 'Geita', 'Iringa', 'Kagera', 'Katavi', 'Kigoma', 'Kilimanjaro', 'Lindi', 'Manyara', 'Mara', 'Mbeya', 'Morogoro', 'Mtwara', 'Mwanza', 'Njombe', 'Pemba Kaskazini','Pemba Kusini', 'Pwani', 'Rukwa', 'Ruvuma', 'Shinyanga', 'Simiyu', 'Singida', 'Songwe', 'Tabora', 'Tanga', 'Unguja Kaskazini', 'Unguja Mjini Magharibi', 'Unguja Kusini', 'Sokoto', 'Taraba', 'Yobe', 'Zamfara','Kilimanjaro', 'Lindi',
+        ];
+
+        foreach ($states as $state) {
+            State::create(['name' => $state]);
+        }
+    }
+
+}
